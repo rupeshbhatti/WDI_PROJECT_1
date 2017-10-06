1b2fb97826198db49210ddbf5b875afce5c5cd9b
rupeshbhatti <rupesh.bhatti@hotmail.com>
2017-10-06 10:54:17 +0100

    Boiler plate added

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('working');
+
+// flappy bird
+
+// Welcome screen with instructions
+// On page load, ask the player if they are ready - press a start button to start (initialise game)
+// Once start button has been clicked, show flappy turd starting to fall (add click event handler on load)
+// fly each time mouse clicked
+// fall each time mouse not clicked
+// Display animated obstacles moving from right of screen to left
+// Win logic - increment score each time an obstacle is successfully passed
+// Collision logic + game over / restart
+// Animations / character + graphics (libraries such as phaser.io)
+//
+// modules:
+//
+// displayWelcomeScreen
+// startGame
+// fall
+// fly
+// displayObstacle
+// animateObstacles
+// incrementScore
+// collision
+// gameOver
+// restart (which is the same as startGame)
